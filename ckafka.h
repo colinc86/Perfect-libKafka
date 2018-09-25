38936847118386b370e32ab98f96da02ebf30f48
Colin Campbell <colinc86@gmail.com>
2018-09-25 12:00:09 -0500

    Revert all changes.

--- ckafka.h
+++ ckafka.h
@@ -1,8 +1,8 @@
 #ifndef __PERFECT_CLIB_RD_KAFKA__
 #define __PERFECT_CLIB_RD_KAFKA__
 #ifdef __APPLE__
-#include "/usr/local/include/librdkafka/rdkafkacpp.h"
+#include "/usr/local/include/librdkafka/rdkafka.h"
 #else
-#include "/usr/include/librdkafka/rdkafkacpp.h"
+#include "/usr/include/librdkafka/rdkafka.h"
 #endif
 #endif
